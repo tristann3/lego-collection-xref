c19194d87b4348abe7233e0bb960a5f62c0beced
RogueNPC <jordanbtansay@gmail.com>
2021-02-03 12:12:36 -0800

    small change

--- routes.py
+++ routes.py
@@ -2,7 +2,6 @@ from flask import Flask, request, redirect, render_template, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from pymongo import MongoClient
-import os
 import pymongo
 
 """Modeled after Web1.1 plant database assignment"""
