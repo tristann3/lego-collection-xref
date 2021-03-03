84c383d9bb8255ff5c00df15de04ea134cd6b065
Tristan Thompson <tristan15thompson@gmail.com>
2021-03-02 21:58:12 -0800

    [UPDATE] updated database connection for Postgres

--- legos_app/config.py
+++ legos_app/config.py
@@ -7,6 +7,6 @@ load_dotenv()
 class Config(object):
     """Set environment variables."""
 
-    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY')
